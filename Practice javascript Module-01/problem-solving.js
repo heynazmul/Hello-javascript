3697cec9e4ec13db0c44ff7f919da7b3dcd836eb
Nazmul Hossain <heynazmulhossain@gmail.com>
2023-03-30 16:39:24 +0600

    problem solving

---
+++ Practice javascript Module-01/problem-solving.js
@@ -0,0 +1,4 @@
+
+// **Problem No. 1
+
+//  Harry's mom gave him money and asked him to buy some oranges and apples. write a program to help harry calculate how much money the shopkeeper will return?
