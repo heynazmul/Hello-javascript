920c13c10ad8d0b7fc12f9e070a31cfabcbacddb
Nazmul Hossain <heynazmulhossain@gmail.com>
2023-03-30 11:54:58 +0600

    type.js

--- Practice javascript/typs.js
+++ Practice javascript/typs.js
@@ -4,7 +4,14 @@ var price = '100';
 // console.log(typeof price);
 
 var isHappy = true;
-console.log(typeof isHappy);
+// console.log(typeof isHappy);
 
 var romantice;
-console.log(typeof romantice);      
+// console.log(typeof romantice);      
+
+var num1 = 0.1;
+var num2 = 0.3;
+var sum = num1 + num2;
+sum = sum.toFixed(2)
+sum = parseFloat(sum);
+console.log(sum);
