0b5a26dfb77f8ddc4216b90f3a5d5dbfe3e30c3f
Nazmul Hossain <heynazmulhossain@gmail.com>
2023-03-30 16:48:17 +0600

    problem-01

--- Practice javascript Module-01/problem-solving.js
+++ Practice javascript Module-01/problem-solving.js
@@ -2,3 +2,11 @@
 // **Problem No. 1
 
 //  Harry's mom gave him money and asked him to buy some oranges and apples. write a program to help harry calculate how much money the shopkeeper will return?
+
+var money = 1000;
+var oranges = 500;
+var apples = 350;
+var totalPrice = oranges + apples;
+var returnMoney = totalPrice - money;
+
+console.log(returnMoney);
