06960e114220c383387815fe7a4efc813ca7eaf9
Nazmul Hossain <heynazmulhossain@gmail.com>
2023-03-31 00:36:01 +0600

    problem solving

--- Practice javascript Module-01/problem-solving.js
+++ Practice javascript Module-01/problem-solving.js
@@ -9,4 +9,31 @@ var apples = 350;
 var totalPrice = oranges + apples;
 var returnMoney = totalPrice - money;
 
-console.log(returnMoney);
+// console.log(returnMoney);
+
+
+// problem Srah's mother is teaching her mathmetics, she gave sarah a number and asked her what the reminder would be if she divided the number by 5. help sarah write the program
+
+sum = 119 % 5 ;
+
+console.log(sum);
+
+
+// **problem 3. 
+
+var bangla = 75.25;
+var math = 65;
+var bio = 80;
+var chem = 35.45;
+var phy = 99.50;
+
+var sum = bangla + math + bio + chem + phy;
+
+var avg = sum / 5;
+
+console.log(avg.toFixed(2));
+
+
+
+
+
