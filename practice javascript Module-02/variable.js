79be187d7ab453461dc1daef36e2ddbe9bc802ce
Nazmul Hossain <heynazmulhossain@gmail.com>
2023-04-05 15:17:57 +0600

    variable.js

---
+++ practice javascript Module-02/variable.js
@@ -0,0 +1,28 @@
+// number type variable
+var sunglassPrice = 120; 
+
+// string type variable
+var specialOneName = "Nazmul Hossain"
+
+// boolean type variable
+var isSingle = true;
+
+// tow types of number: Integer: 120,
+// float, deciaml: 3.65,
+
+
+// variable  naming covnention: rules, best practice
+/**
+ *1. single word
+ *2. No gap or space
+ *3. no quote
+ *4. no keyword
+ *5. can not start start with a number
+ *6. can not contain dash
+ *7. should use camelcase
+ * */ 
+// Math opertaions: +, -, *, /
+// Shorthand: +=, -=, ++, --
+// connvert: perseInt, parseFloat
+// toFixed
+// reinder : vagses: %
