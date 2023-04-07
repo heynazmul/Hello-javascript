f28827957a248b4ae004f000377a0063442c9b5c
Nazmul Hossain <heynazmulhossain@gmail.com>
2023-04-07 16:46:10 +0600

    index.js

---
+++ practice javascript Module-02/index.js
@@ -0,0 +1,11 @@
+var numbers = [45, 65, 78, 56, 89, 98];
+
+// ge element by index
+var element = numbers[0];
+
+console.log(element);
+
+// set element value by index
+numbers[1] = 77;
+numbers[3] = 44;
+console.log(numbers);
