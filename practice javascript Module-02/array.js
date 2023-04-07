b2c1cb1259a7d4b605c49ff52db79e7d6c9e8ac3
Nazmul Hossain <heynazmulhossain@gmail.com>
2023-04-07 16:31:40 +0600

    arry.js

---
+++ practice javascript Module-02/array.js
@@ -0,0 +1,8 @@
+// Arry
+
+var friendsAge = [11, 21, 45, 17, 14, 10, 5, 6];
+var picnicFee = [5000, 2000, 4000, 150];
+var mofizerDol = ['abul', 'kuddus', 'mokku'];
+var vowels = ['a', 'e', 'i', '0', 'u'];
+
+console.log(friendsAge.length);
